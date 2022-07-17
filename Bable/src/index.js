d0cc39bd106543035ccfec2b39ea535a0851b0e7
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2022-07-17 16:47:02 +0530

    Modified babel to reload automatically and convert file in index.js to older in bundle.js

---
+++ Bable/src/index.js
@@ -0,0 +1,12 @@
+const greet = (name)=>{
+    console.log(`hello ${name}`);
+};
+greet('ashu');
+greet('abhi');
+greet('anur');
+
+class User{
+    constructor(){
+        this.score = 10000;
+    }
+}
